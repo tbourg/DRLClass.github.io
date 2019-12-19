b68610f351077c3b21bc4f72252a6c9d1f6861fa
Thomas Bourg <tbourg07@gmail.com>
2019-12-19 17:50:45 +0100

    Tp 2

--- cartpole.py
+++ cartpole.py
@@ -2,15 +2,19 @@ import argparse
 import sys
 import matplotlib.pyplot as plt
 import random
+import copy
 
 import gym
 from gym import wrappers, logger
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
+import torch.cuda
 
 BUFFER_SIZE = 10000
-MINI_BATCH_SIZE = 100
-EPSILON = 1
+MINI_BATCH_SIZE = 32
+EPSILON = .1
+GAMMA = .01
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -19,48 +23,74 @@ class RandomAgent(object):
         self.buffer = []
         self.exploration_method = exploration_method
         self.brain = MLP(env.observation_space.shape[0], env.action_space.n)
+        self.brain_bis = copy.deepcopy(self.brain)
+        self.cpt = 0
 
     def act(self, ob, reward, done):
         if self.exploration_method == 'epsilon_greedy':
             if random.random() < EPSILON:
                 return self.action_space.sample()
             else:
-                self.brain.eval()
-                with torch.no_grad():
-                    output = self.brain(ob)
-                    index = torch.max(output, 1)
-                return self.action_space[i]
+                return self.get_best_action(ob)
     
     def learn(self, prev_ob, action, ob, reward, done):
         interaction = (prev_ob, action, ob, reward, done)
         self.buffer.append(interaction)
         if len(self.buffer) > BUFFER_SIZE:
             self.buffer.pop(0)
+        minibatch = self.get_minibatch()
+        for interaction in minibatch:
+            self.cpt += 1
+            if self.cpt % 100:
+                self.brain_bis = copy.deepcopy(self.brain)
+            q_vals_pred = self.brain(torch.tensor(interaction[0]).float())
+            q_vals_pred_bis = self.brain_bis(torch.tensor(interaction[0]).float())
+            q_vals_pred_next = self.brain_bis(torch.tensor(interaction[2]).float())
+            q_vals = [None for _ in range(self.action_space.n)]
+            if interaction[4]:
+                for i in range(self.action_space.n):
+                    q_vals[i] = (q_vals_pred_bis[i] - interaction[3]) ** 2
+            else:
+                for i in range(self.action_space.n):
+                    q_vals[i] = (q_vals_pred_bis[i] - (interaction[3] + GAMMA * torch.max(q_vals_pred_next).item()))
+            self.brain.train()
+            loss = self.brain.loss(q_vals_pred, torch.tensor(q_vals).float())
+            loss = Variable(loss, requires_grad=True)
+            loss.backward()
             
     def get_minibatch(self):
         size = min(len(self.buffer), MINI_BATCH_SIZE)
         return random.sample(self.buffer, k=size)
     
     def get_q_val(self, ob, action):
-        pass
+        return self.get_q_vals(ob)[action]
         
     def get_best_action(self, ob):
-        pass
+        index = torch.argmax(self.get_q_vals(ob), 0)
+        return index.int().item()
+        
+    def get_max_q_val(self, ob):
+        val, _ = torch.max(self.get_q_vals(ob), 0)
+        return val.float().item()
         
     def get_q_vals(self, ob):
         self.brain.eval()
         with torch.no_grad():
+            ob = torch.tensor(ob).float()
             output = self.brain(ob)
         return output
+    
+    
     
 class MLP(nn.Module):
     def __init__(self, input_size, output_size):
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
-            nn.Linear(input_size, 100),
+            nn.Linear(input_size, 10),
             nn.ReLU(),
-            nn.Linear(100, output_size)
+            nn.Linear(10, output_size)
         )
+        self.loss = nn.MSELoss()
         
     def forward(self, x):
         x = self.layers(x)
@@ -77,7 +107,6 @@ if __name__ == '__main__':
 
     env = gym.make(args.env_id)
     rewards = []
-    print(env.action_space.n)
 
     #env = wrappers.Monitor(env, force=True)
     #env.seed(0)
@@ -98,7 +127,7 @@ if __name__ == '__main__':
             prev_ob = ob
             episode_reward += reward
             if done:
-                print(ob, action)
+                print(i, episode_reward)
                 rewards.append(episode_reward)
                 break
     print(rewards)
